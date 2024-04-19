403d628d1ab2630a9f65b82f36a87afca123d090
~~Leo <ardileon444@gmail.com>
2024-04-19 21:09:42 +0700

    add

---
+++ src/main/java/com/cb/dckpst/DckpstApplication.java
@@ -0,0 +1,13 @@
+package com.cb.dckpst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DckpstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DckpstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cb/dckpst/DckpstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cb.dckpst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DckpstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
